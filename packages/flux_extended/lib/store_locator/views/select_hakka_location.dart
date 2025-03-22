5b30df58979c4b69a385da28a11a04f9fc8e843d
empash938 <ashokthapa938@gmail.com>
2025-03-22 20:31:03 +0545

    location added for hakka

--- packages/flux_extended/lib/store_locator/views/select_hakka_location.dart
+++ packages/flux_extended/lib/store_locator/views/select_hakka_location.dart
@@ -17,67 +17,81 @@ class SelectHakkaLocation extends StatefulWidget {
 }
 
 class _SelectHakkaLocationState extends State<SelectHakkaLocation> {
-  // final _services = StoreLocatorServices();
-  // List<Store> _stores = [];
+  final _services = StoreLocatorServices();
+  List<Store> _stores = [];
   late ShippingType _shippingType =
       Provider.of<CartModel>(context, listen: false).shippingType;
-  // late Store? _selectedStore =
-  //     Provider.of<CartModel>(context, listen: false).selectedStore;
+  late Store? _selectedStore =
+      Provider.of<CartModel>(context, listen: false).selectedStore;
+
+  Future<void> setStore() async {
+    try {
+      final store = await SaveStoreLocation.getStore();
+      if (store != null) {
+        setState(() {
+          _selectedStore = store;
+        });
+      }
+    } catch (e) {
+      print('$e');
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-    // Future.delayed(Duration.zero, () {
-    //   _services.getStores(showAll: true).then((stores) {
-    //     setState(() {
-    //       _stores = stores;
-    //     });
-    //   });
-    // });
+    setStore();
+    Future.delayed(Duration.zero, () {
+      _services.getStores(showAll: true).then((stores) {
+        setState(() {
+          _stores = stores;
+        });
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    // if (_stores.isEmpty) return const SizedBox();
+    if (_stores.isEmpty) return const SizedBox();
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const SizedBox(height: 15),
-        // renderStoreInput(),
-        // if (_selectedStore != null)
-        //   Container(
-        //     width: double.infinity,
-        //     padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-        //     margin: const EdgeInsets.symmetric(vertical: 15),
-        //     decoration: BoxDecoration(
-        //       borderRadius: BorderRadius.circular(4.0),
-        //       color: const Color(0xffEEEEED),
-        //     ),
-        //     child: (_shippingType == ShippingType.pickup)
-        //         ? Column(
-        //             crossAxisAlignment: CrossAxisAlignment.start,
-        //             children: [
-        //               Text(
-        //                 '${S.of(context).addressToPickup} :',
-        //                 style: Theme.of(context)
-        //                     .textTheme
-        //                     .titleLarge
-        //                     ?.copyWith(fontWeight: FontWeight.bold),
-        //               ),
-        //               const SizedBox(height: 10),
-        //               HtmlWidget(_selectedStore?.shippingAddress ?? ''),
-        //             ],
-        //           )
-        //         : Text(
-        //             S.of(context).comingSoon,
-        //             style: Theme.of(context)
-        //                 .textTheme
-        //                 .titleLarge
-        //                 ?.copyWith(fontWeight: FontWeight.bold),
-        //           ),
-        //   ),
-        // const SizedBox(height: 15),
+        renderStoreInput(),
+        if (_selectedStore != null)
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+            margin: const EdgeInsets.symmetric(vertical: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4.0),
+              color: const Color(0xffEEEEED),
+            ),
+            child: (_shippingType == ShippingType.pickup)
+                ? Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '${S.of(context).addressToPickup} :',
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleLarge
+                            ?.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 10),
+                      HtmlWidget(_selectedStore?.shippingAddress ?? ''),
+                    ],
+                  )
+                : Text(
+                    S.of(context).comingSoon,
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge
+                        ?.copyWith(fontWeight: FontWeight.bold),
+                  ),
+          ),
+        const SizedBox(height: 15),
         Text(S.of(context).pickupOrDelivery,
             style: Theme.of(context).textTheme.titleMedium),
         ...ShippingType.values.map((e) => Row(
@@ -100,33 +114,33 @@ class _SelectHakkaLocationState extends State<SelectHakkaLocation> {
     );
   }
 
-  // Widget renderStoreInput() {
-  //   var items = <DropdownMenuItem>[];
-  //   for (var item in _stores) {
-  //     items.add(
-  //       DropdownMenuItem(
-  //         value: item.slug,
-  //         child: Text(item.name!),
-  //       ),
-  //     );
-  //   }
+  Widget renderStoreInput() {
+    var items = <DropdownMenuItem>[];
+    for (var item in _stores) {
+      items.add(
+        DropdownMenuItem(
+          value: item.slug,
+          child: Text(item.name!),
+        ),
+      );
+    }
 
-  //   return DropdownButtonFormField<dynamic>(
-  //     items: items,
-  //     value: _selectedStore?.slug,
-  //     validator: (val) {
-  //       return null;
-  //     },
-  //     onChanged: (dynamic val) async {
-  //       setState(() {
-  //         _selectedStore = _stores.firstWhereOrNull((e) => e.slug == val);
-  //       });
-  //       Provider.of<CartModel>(context, listen: false)
-  //           .selectStore(_selectedStore);
-  //     },
-  //     isExpanded: true,
-  //     itemHeight: 70,
-  //     hint: Text(S.of(context).selectHakkaLocation),
-  //   );
-  // }
+    return DropdownButtonFormField<dynamic>(
+      items: items,
+      value: _selectedStore?.slug,
+      validator: (val) {
+        return null;
+      },
+      onChanged: (dynamic val) async {
+        setState(() {
+          _selectedStore = _stores.firstWhereOrNull((e) => e.slug == val);
+        });
+        Provider.of<CartModel>(context, listen: false)
+            .selectStore(_selectedStore);
+      },
+      isExpanded: true,
+      itemHeight: 70,
+      hint: Text(S.of(context).selectHakkaLocation),
+    );
+  }
 }
